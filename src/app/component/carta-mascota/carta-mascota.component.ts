af8dda3b402ec9bf3ef8b7847d3892ce52710c34
laucadi <lauracaicedo182@gmail.com>
2022-06-15 20:26:51 -0500

    ultimos cambios

--- src/app/component/carta-mascota/carta-mascota.component.ts
+++ src/app/component/carta-mascota/carta-mascota.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Input } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { UsuarioService } from 'src/app/sevicios/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carta-mascota',
   templateUrl: './carta-mascota.component.html',
-  styleUrls: ['./carta-mascota.component.css']
+  styleUrls: ['./carta-mascota.component.css'],
 })
 export class CartaMascotaComponent implements OnInit {
   @Input() mascota: any = {};
   @Input() botonDeAdopcion: boolean = false;
   @Output() onEliminarMascota = new EventEmitter<any>();
   public base_url = environment.url;
+  public rol: any;
+  public token: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private usuarioService: UsuarioService) {
+    this.rol = this.usuarioService.obtenerRol();
+    console.log(this.rol);
+    this.token = this.usuarioService.obtenerToken();
   }
+
+  ngOnInit(): void {}
 
   public calcularEdad(fecha: string) {
     const fechaDeNacimiento = new Date(fecha);
@@ -32,8 +39,11 @@ export class CartaMascotaComponent implements OnInit {
     if (meses === 0) return `${anos} años`;
     return `${anos} años y ${meses} meses`;
   }
-  
-  public eliminarMascota () {
-    this.onEliminarMascota.emit()
+
+  public eliminarMascota() {
+    this.onEliminarMascota.emit();
+  }
+  public boton() {
+    Swal.fire('Debes inicar sesion para poder realizar el proceso de adopción');
   }
 }
--- src/app/component/listado-publico/listado-publico.component.ts
+++ src/app/component/listado-publico/listado-publico.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { MascotaService } from 'src/app/sevicios/mascota.service';
-
+import { UsuarioService } from 'src/app/sevicios/usuario.service';
 
 @Component({
   selector: 'app-listado-publico',
   templateUrl: './listado-publico.component.html',
-  styleUrls: ['./listado-publico.component.css']
+  styleUrls: ['./listado-publico.component.css'],
 })
 export class ListadoPublicoComponent implements OnInit {
   public mascotas = [];
+  rol: any;
 
   constructor(
-    private mascotaService: MascotaService
-  ) { }
+    private mascotaService: MascotaService,
+    private usuarioService: UsuarioService
+  ) {}
 
   ngOnInit(): void {
     this.cargarMascotas();
@@ -20,8 +22,7 @@ export class ListadoPublicoComponent implements OnInit {
 
   cargarMascotas() {
     this.mascotaService.listarMascotas().subscribe((response) => {
-      this.mascotas = response
-    })
+      this.mascotas = response;
+    });
   }
-
 }
